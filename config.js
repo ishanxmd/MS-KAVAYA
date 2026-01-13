a45f74c9d5bde4834d75bc774b3e9ef50d220e34
ishanxmd <ishanmadusanke548@gmail.com>
2026-01-13 16:38:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "f5IQ3AoK#yaGlFic8BA42HFkn6udIaJZj87_zQo91yeLSZPrvTIs",
+SESSION_ID: process.env.SESSION_ID || "f5gy1AIR#FP5S8LFsoQSxuaBOu6Ok9D3dlY37AnI0Lwu2WZAyIpo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/d0z6ym.JPG",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 ISHAN-MD Is Alive Now😍*",
 BOT_OWNER: '94761638379',  // Replace with the owner's phone number
