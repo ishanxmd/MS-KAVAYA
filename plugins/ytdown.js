f567be11eaa89bbd85c76ddda32c4ec2f5dccc08
ishanxmd <ishanmadusanke548@gmail.com>
2026-01-15 17:37:54 +0530

    Update ytdown.js

--- plugins/ytdown.js
+++ plugins/ytdown.js
@@ -1,6 +1,6 @@
 const {cmd , commands} = require('../command')
 const yts = require('yt-search');
-const fg = require('api-dylux');
+const fg = require('sadaslk-dlcore');
 
 // -------- Song Download --------
 cmd({
